b86270e10ecfb730efd3ef34a22ae1336ca8c6af
Deckiaron Consultant Ltd <fowusu-agyemang@deckiarons-mbp.home>
2020-12-18 16:15:10 +0000

    update travis.yaml file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t frannkyoa/multi-client:latest -t frannkyoa/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t frannkyoa/multi-server:latest -t frannkyoa/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t frannkyoa/multi-worker:latest -t frannkyoa/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push frannkyoa/multi-client:latest
-docker push frannkyoa/multi-server:latest
-docker push frannkyoa/multi-worker:latest
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
 
-docker push frannkyoa/multi-client:$SHA
-docker push frannkyoa/multi-server:$SHA
-docker push frannkyoa/multi-worker:$SHA
+docker push stephengrider/multi-client:$SHA
+docker push stephengrider/multi-server:$SHA
+docker push stephengrider/multi-worker:$SHA
 
 kubectl apply -f /k8s
-kubectl set image deployments/server-deployment server=frannkyoa/multi-server:$SHA
-kubectl set image deployments/client-deployment client=frannkyoa/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=frannkyoa/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
